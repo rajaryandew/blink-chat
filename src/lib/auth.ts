2db8936b75b363441651f4a1b79a7c11e8a55aac
rajaryandew <raajaaryandewangan@gmail.com>
2025-10-01 01:29:07 +0530

    setup(auth): Auth client setup done and google oauth done changes(initial route): removed boilerplate code and cleaned the structure

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "../../generated/prisma";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "../config";
 
 const prisma = new PrismaClient();
 export const auth = betterAuth({
+    socialProviders:{
+        google:{
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET
+        }
+    },
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
