3cfab5d53f5969630a1816badbc90b2a8100f1e7
rajaryandew <raajaaryandewangan@gmail.com>
2025-10-05 00:28:10 +0530

    fix prisma import errors in deployment

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./config";
 
 const prisma = new PrismaClient();
