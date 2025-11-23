e987c8ba8cdfa0fe93a65b7199c8f479d125f0a1
rajaryandew <raajaaryandewangan@gmail.com>
2025-11-23 23:15:11 +0530

    remove unnecessary files

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,4 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
@@ -29,7 +28,6 @@ export default function Home() {
           alt="Turborepo logo"
           width={180}
           height={38}
-          priority
         />
         <ol>
           <li>
@@ -63,9 +61,6 @@ export default function Home() {
             Read our docs
           </a>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
       </main>
       <footer className={styles.footer}>
         <a
