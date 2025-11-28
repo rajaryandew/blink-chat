6ab44b92a8067402a065bd9f38e6e3de89f5022d
rajaryandew <raajaaryandewangan@gmail.com>
2025-11-28 23:00:27 +0530

    setup auth client and google oauth

--- apps/web/lib/auth.ts
+++ apps/web/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@repo/database/prisma-client";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "./config";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -10,6 +11,9 @@ export const auth = betterAuth({
         enabled:true
     },
     socialProviders:{
-        
+        google:{
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret:GOOGLE_CLIENT_SECRET
+        }
     }
 });
