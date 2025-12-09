2625ec4e9f1c9b02020f7d506f90b67dbeb11710
rajaryandew <raajaaryandewangan@gmail.com>
2025-12-09 21:01:45 +0530

    tweaks(web): Minor ui changes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,14 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { socialSignIn } from "@/lib/auth/sign-in";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <div>
             <h1>hello</h1>
-            <Button onClick={async () => await socialSignIn("google")}>
-                google login
-            </Button>
+            <Link href="/auth/login">Signup</Link>
         </div>
     );
 }
--- apps/web/src/components/login/SVGs.tsx
+++ apps/web/src/components/login/SVGs.tsx
@@ -6,9 +6,7 @@ export function GoogleIcon() {
             preserveAspectRatio="xMidYMid"
         >
             <g id="SVGRepo_bgCarrier"></g>
-            <g
-                id="SVGRepo_tracerCarrier"
-            ></g>
+            <g id="SVGRepo_tracerCarrier"></g>
             <g id="SVGRepo_iconCarrier">
                 <path
                     d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
