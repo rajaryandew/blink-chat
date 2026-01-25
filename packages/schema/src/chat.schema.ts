ac9931a2aa01c8098debed2e2b9af5ecd3a2bdae
rajaryandew <raajaaryandewangan@gmail.com>
2026-01-25 15:39:07 +0530

    move files

--- packages/schema/src/chat.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as z from "zod"
-
-export const chatSchema = z.object({
-    id: z.uuid(),
-    chatName:z.string().max(50),
-    isGroup:z.boolean(),
-})
-export type Chat = z.infer<typeof chatSchema>;
